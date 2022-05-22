278e19319805c41d497f9610294306f17b60e364
Erik Sargazakov <erik@serpnet.org>
2022-05-22 18:25:46 +0200

    Add blank Symfony command

---
+++ src/Command/WorkshopRockItCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'workshop:rock-it',
+    description: 'No description for you',
+)]
+class WorkshopRockItCommand extends Command
+{
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->warning('Assuming direct control.');
+
+        return Command::SUCCESS;
+    }
+}
